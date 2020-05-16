ee935c8324f4200509f00ad0bf77b49478cbbd58
Visal Rajapakse <visalrajapakse@gmail.com>
2020-05-16 16:18:29 +0530

    Completed the Xylophone tutorial

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,36 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
-
     
-
+    
+    @IBAction func keyCPressed(_ sender: UIButton) {
+        //getting the button label
+        let buttonLabel : String = sender.currentTitle!
+        //passing the value to the playsound function
+        playSound(soundName: buttonLabel)
+    }
+    
+    //function to play the sound
+    func playSound(soundName: String){
+        //https://www.hackingwithswift.com/example-code/media/how-to-play-sounds-using-avaudioplayer
+        let soundPath = Bundle.main.path(forResource: "\(soundName).wav", ofType: nil)!
+        let url = URL(fileURLWithPath: soundPath)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
+//            print("played the sound")
+        } catch {
+            print("Failed to play the sound")
+        }
+    }
 }
 
